64883de429255d19c0894f442cf6efb5fc28a0ec
Shelton Suen <sheltonsuen@gmail.com>
2020-05-16 10:15:11 +0800

    [Maoning | Xiaocong] - should save luggage to first available locker when deposit a luggage given a robot

--- src/entities/__tests__/robot.test.js
+++ src/entities/__tests__/robot.test.js
@@ -12,3 +12,20 @@ test('should return a ticket when deposit a luggage given a robot', () => {
     password: 'foo',
   });
 });
+
+test('should save luggage to first available locker when deposit a luggage given a robot', () => {
+  Math.random = jest.fn().mockReturnValue('foo');
+  const robot = new Robot();
+  robot.deposit({});
+  robot.deposit({});
+  robot.deposit({});
+  robot.deposit({});
+
+  const ticket = robot.deposit({});
+
+  expect(ticket).toEqual({
+    lockerNumber: 1,
+    number: 0,
+    password: 'foo',
+  });
+});
--- src/entities/robot.js
+++ src/entities/robot.js
@@ -1,13 +1,25 @@
 import Locker from './locker';
 
+const DEFAULT_LOCKER_SIZE = 4;
+
 export default class Robot {
   constructor() {
-    this.lockers = [new Locker(4)];
+    this.lockers = [new Locker(DEFAULT_LOCKER_SIZE), new Locker(DEFAULT_LOCKER_SIZE)];
   }
 
   deposit(luggage) {
-    const ticket = this.lockers[0].deposit(luggage);
+    return this.lockers.reduce((acc, locker, index) => {
+      if (acc) {
+        return acc;
+      }
 
-    return { ...ticket, lockerNumber: 0 }
+      const result = locker.deposit(luggage);
+
+      if (result.number != null) {
+        acc = { ...result, lockerNumber: index };
+      }
+
+      return acc;
+    }, undefined);
   }
 }
