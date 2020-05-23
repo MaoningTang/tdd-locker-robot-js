6d423b19665079e862b67b7ba61be8fa33db30fb
Shelton Suen <sheltonsuen@gmail.com>
2020-05-23 10:03:09 +0800

    [Maoning | Xiaocong] - refactor by renaming to primaryRobot

--- src/entities/__tests__/primaryRobot.test.js
+++ src/entities/__tests__/primaryRobot.test.js
@@ -1,4 +1,4 @@
-import Robot from '../robot';
+import PrimaryRobot from '../primaryRobot';
 import Locker from '../locker';
 
 let realRandom;
@@ -12,8 +12,8 @@ afterEach(() => {
   Math.random = realRandom;
 });
 
-test('should return a ticket when deposit a luggage given a robot', () => {
-  const robot = new Robot([new Locker(1)]);
+test('should return a ticket when deposit a luggage given a primary robot', () => {
+  const robot = new PrimaryRobot([new Locker(1)]);
 
   const ticket = robot.deposit({});
 
@@ -24,11 +24,11 @@ test('should return a ticket when deposit a luggage given a robot', () => {
   });
 });
 
-test('should save luggage in order when deposit a luggage given a robot with available locker', () => {
+test('should save luggage in order when deposit a luggage given a primary robot with available locker', () => {
   const fullLocker = new Locker(1);
   fullLocker.deposit({});
   const availableLocker = new Locker(1);
-  const robot = new Robot([fullLocker, availableLocker]);
+  const robot = new PrimaryRobot([fullLocker, availableLocker]);
   const expectedSavedLuggage = { foo: 'bar' };
 
   const ticket = robot.deposit(expectedSavedLuggage);
@@ -36,8 +36,8 @@ test('should save luggage in order when deposit a luggage given a robot with ava
   expect(availableLocker.pickup(ticket)).toEqual(expectedSavedLuggage);
 });
 
-test('should return a full locker message when user deposit a luggage given a robot without available locker', () => {
-  const robot = new Robot([new Locker(1)]);
+test('should return a full locker message when user deposit a luggage given a primary robot without available locker', () => {
+  const robot = new PrimaryRobot([new Locker(1)]);
   robot.deposit({});
 
   const ticket = robot.deposit({});
@@ -45,8 +45,8 @@ test('should return a full locker message when user deposit a luggage given a ro
   expect(ticket).toEqual('The lockers are full.');
 });
 
-test('should return the correct luggage when user pickup given a robot and a valid ticket', () => {
-  const robot = new Robot([new Locker(1)]);
+test('should return the correct luggage when user pickup given a primary robot and a valid ticket', () => {
+  const robot = new PrimaryRobot([new Locker(1)]);
   const ticket = robot.deposit({ foo: 'bag' });
 
   const luggage = robot.pickup(ticket);
@@ -54,8 +54,8 @@ test('should return the correct luggage when user pickup given a robot and a val
   expect(luggage).toEqual({ foo: 'bag' });
 });
 
-test('should return a invalid ticket message when user pickup given a robot and a invalid ticket', () => {
-  const robot = new Robot([new Locker(1)]);
+test('should return a invalid ticket message when user pickup given a primary robot and a invalid ticket', () => {
+  const robot = new PrimaryRobot([new Locker(1)]);
   robot.deposit({});
 
   const message = robot.pickup({
