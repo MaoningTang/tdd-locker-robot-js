e5bb87ce01f12c4c44cedf79772d7c033346e1e5
Shelton Suen <sheltonsuen@gmail.com>
2020-05-23 10:06:49 +0800

    [Maoning | Xiaocong] - duplicate primaryRobot as smartRobot

---
+++ src/entities/smartRobot.js
@@ -0,0 +1,38 @@
+export default class SmartRobot {
+  constructor(lockers) {
+    this.lockers = lockers;
+  }
+
+  deposit(luggage) {
+    const result = depositLuggageToAvailableLocker(this.lockers, luggage);
+
+    if (!result) {
+      return 'The lockers are full.'
+    }
+
+    return result;
+  }
+
+  pickup(ticket) {
+      if (!this.lockers[ticket.lockerNumber]) {
+          return 'Invalid Ticket';
+      }
+
+      return this.lockers[ticket.lockerNumber].pickup(ticket);
+  }
+}
+
+const depositLuggageToAvailableLocker = (lockers, luggage) =>
+    lockers.reduce((acc, locker, index) => {
+      if (acc) {
+        return acc;
+      }
+
+      const result = locker.deposit(luggage);
+
+      if (result.number != null) {
+        acc = { ...result, lockerNumber: index };
+      }
+
+      return acc;
+    }, undefined);
